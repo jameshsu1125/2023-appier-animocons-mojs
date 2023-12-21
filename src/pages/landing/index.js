192d99e3bd29bf08ec9716d3a61f6a23117ee153
jameshsu1125 <jameshsu1125@gmail.com>
2023-12-21 14:31:53 +0800

    Update index.js

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -15,44 +15,24 @@ const Landing = memo(() => {
 		effects.current = [
 			new mojs.Burst({
 				parent: ref.current,
-				count: 6,
-				radius: { 40: 90 },
+				count: 15,
+				radius: { 20: 80 },
+				rotate: { 0: 140, easing: mojs.easing.bezier(0.1, 1, 0.3, 1) },
 				children: {
-					fill: ['#988ADE', '#DE8AA0', '#8AAEDE', '#8ADEAD', '#DEC58A', '#8AD1DE'],
+					fill: '#988ADE',
+					radius: 20,
 					opacity: 0.6,
-					scale: 1,
-					radius: { 7: 0 },
 					duration: 1500,
-					delay: 300,
 					easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
 				},
 			}),
-			new mojs.Shape({
-				parent: ref.current,
-				type: 'circle',
-				scale: { 0: 1 },
-				radius: 50,
-				fill: 'transparent',
-				stroke: '#988ADE',
-				strokeWidth: { 35: 0 },
-				opacity: 0.6,
-				duration: 750,
-				easing: mojs.easing.bezier(0, 1, 0.5, 1),
-			}),
+			// icon scale animation
 			new mojs.Tween({
-				duration: 1100,
-				onUpdate: (progress) => {
-					const target = imgRef.current;
-					if (progress > 0.3) {
-						const elasticOutProgress = mojs.easing.elastic.out(1.43 * progress - 0.43);
-						if (target) {
-							target.style.WebkitTransform = `scale3d(${elasticOutProgress},${elasticOutProgress},1)`;
-							target.style.transform = `scale3d(${elasticOutProgress},${elasticOutProgress},1)`;
-						}
-					} else {
-						target.style.WebkitTransform = 'scale3d(0,0,1)';
-						target.style.transform = 'scale3d(0,0,1)';
-					}
+				duration: 800,
+				easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+				onUpdate: function (progress) {
+					imgRef.current.style.WebkitTransform = imgRef.current.style.transform =
+						'scale3d(' + progress + ',' + progress + ',1)';
 				},
 			}),
 		];
